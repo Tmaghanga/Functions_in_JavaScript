5e2e58a07b2b994f9bb012b02fb9464703f03813
maghanga <maghanga75@gmail.com>
2023-06-06 13:15:12 +0300

    first commit

---
+++ App.js
@@ -0,0 +1,13 @@
+let exclusivePriceComputer = 50000;
+let taxRatePrice = 2000;
+let inclusivePrice = exclusivePriceComputer + taxRatePrice;
+console.log(inclusivePrice);
+
+function calculateInclusivePrice() {
+  let exclusivePriceComputer = 50000;
+  let taxRatePrice = 2000;
+  let inclusivePrice = exclusivePriceComputer + taxRatePrice;
+  console.log(inclusivePrice);
+}
+
+calculateInclusivePrice();
